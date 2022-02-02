36c3a3ff823c23f75fb17abb372b47c087b33707
jona <jona_jko@hotmail.com>
2022-02-01 18:28:27 -0600

    Primer commit, creación del template

---
+++ src/js/componentes.js
@@ -0,0 +1,16 @@
+import '../css/componentes.css';
+import webpackLogo from '../assets/img/webpack-logo.png';
+
+export const saludar = (nombre) => {
+    console.log('Creando etiqueta H1');
+
+    const h1 = document.createElement('h1');
+    h1.innerText = `Hola, ${nombre}!!! Ah PRRO!`;
+
+    document.body.append(h1);
+
+    // Img
+    // const img = document.createElement('img');
+    // img.src = webpackLogo;
+    // document.body.append(img);
+}
